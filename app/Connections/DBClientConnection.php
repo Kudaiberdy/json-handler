1f0b0c4c3e4b0f105d02da24aa5a286acb969ece
Kudaiberdy <kudaiberdy.shagirov@gmail.com>
2022-08-24 15:26:49 +0600

    connect to db-client service

---
+++ app/Connections/DBClientConnection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Connections;
+
+use Illuminate\Support\Facades\Http;
+
+class DBClientConnection
+{
+    private $host;
+
+    public function __construct(string $pathToConfig)
+    {
+        $config = parse_ini_file($pathToConfig);
+        $server = $config['server'];
+        $port = $config['port'];
+
+        $this->host = "{$server}:{$port}";
+    }
+    public function index($key, $value)
+    {
+        $uri = "{$this->host}/index?{$key}={$value}";
+        $response = Http::get($uri);
+
+        return $response->json();
+    }
+}
